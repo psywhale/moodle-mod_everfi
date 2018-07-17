8951fe658226a1e3ba137e6027df7f2f55d5f447
Brian Carpenter <brian.carpenter@wosc.edu>
2018-07-17 17:34:23 -0500

    initial

---
+++ lang/en/everfi.php
@@ -0,0 +1,42 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * English strings for everfi
+ *
+ * You can have a rather longer description of the file as well,
+ * if you like, and it can span multiple lines.
+ *
+ * @package    mod_everfi
+ * @copyright  2016 Your Name <your@email.address>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['modulename'] = 'everfi';
+$string['modulenameplural'] = 'everfis';
+$string['modulename_help'] = 'Use the everfi module for... | The everfi module allows...';
+$string['everfi:addinstance'] = 'Add a new everfi';
+$string['everfi:submit'] = 'Submit everfi';
+$string['everfi:view'] = 'View everfi';
+$string['everfifieldset'] = 'Custom example fieldset';
+$string['everfiname'] = 'everfi name';
+$string['everfiname_help'] = 'This is the content of the help tooltip associated with the everfiname field. Markdown syntax is supported.';
+$string['everfi'] = 'everfi';
+$string['pluginadministration'] = 'everfi administration';
+$string['pluginname'] = 'everfi';
